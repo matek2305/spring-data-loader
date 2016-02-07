902241b2e2fae7d9fe2dae630de38d77a30da371
Mateusz Urbański <matek2305@gmail.com>
2016-02-07 20:13:39 +0100

    cleanup + some javadocs

--- src/main/java/com/github/matek2305/dataloader/annotations/EnableDataLoader.java
+++ src/main/java/com/github/matek2305/dataloader/annotations/EnableDataLoader.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Import;
 import java.lang.annotation.*;
 
 /**
+ * Enable data loader scanning.
+ * @see DataLoaderConfiguration
  * @author Mateusz Urbański <matek2305@gmail.com>
  */
 @Target(ElementType.TYPE)
